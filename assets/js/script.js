dcaa929e658d3d093de5293ad735eb7520039598
Daniel Lucas <danomiterock@yahoo.com>
2021-03-06 13:52:55 -0700

    add hours and buttons

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var dateNow = moment().format("dddd, MMMM Do YYYY");
+// var timeNow = 
+//target "currentDay" id
+var displayToday = document.getElementById("currentDay");
+//assign date in jumbotron
+displayToday.textContent = dateNow
+        
+        
+        function displayDateandTime() {
+        
+      }
