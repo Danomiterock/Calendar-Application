7910ab790418aecf21b19d2166fe36cef0b5e542
Daniel Lucas <danomiterock@yahoo.com>
2021-03-06 14:47:27 -0700

    add variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,14 @@ var dateNow = moment().format("dddd, MMMM Do YYYY");
 var displayToday = document.getElementById("currentDay");
 //assign date in jumbotron
 displayToday.textContent = dateNow
+var currentTime = moment().format("H")
+var time = ("")
+var past = (time < currentTime)
+var present = (time === currentTime)
+var future = (time > currentTime)        
         
+// function displayDateandTime() {
+//     if currentTime.value = time.value
         
-        function displayDateandTime() {
-        
-      }
+// // }
+console.log(currentTime);
