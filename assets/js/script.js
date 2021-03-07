2ae416d5d012653f0705d1b193e0b76cc8dd04e2
Daniel Lucas <danomiterock@yahoo.com>
2021-03-07 15:55:23 -0700

    attempt to add functions governing saving data

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,69 @@ var displayToday = document.getElementById("currentDay");
 //assign date in jumbotron
 displayToday.textContent = dateNow
 //define the business hours as an array for comparison to surrentTime
-var businessHours = ("#hour-"+["9","10","11","12","13","14","15","16","17"]);
+var businessHours = ("#h"+["9","10","11","12","13","14","15","16","17"]);
 //Define currenTime by the corresponding 24 hour clock in Moment
 var currentTime = moment().format("H")
 //Define oast, present, and future relative to currentTime
 var past = (businessHours[""] < currentTime)
 var present = (businessHours[""] === currentTime)
 var future = (businessHours[""] > currentTime) 
+//make an array targeting the ids in html
+// var hour = [
+//     {
+//         $getElementsById(h9)
+//         value: "9",
+//     }
+//     {
+//         $getElementsById(h10)
+//         value: "10",
+//     }
+//     {
+//         $getElementsById(h11)
+//         value: "11",
+//     }
+//     {
+//         $getElementsById(h12)
+//         value: "12",
+//     }
+//     {
+//         $getElementsById(h13)
+//         value: "13",
+//     }
+//     {
+//         $getElementsById(h14)
+//         value: "14",
+//     }
+//     {
+//         $getElementsById(h15)
+//         value: "15",
+//     }
+//     {
+//         $getElementsById(h16)
+//         value: "16",
+//     }
+//     {
+//         $getElementsById(h17)
+//         value: "17",
+//     }
+// ]
+
+//Make sure everything loads in the page before save button is pressed
+document.addEventListener('load', ()=>{
+    document.getElementsById("#currentDay").display.innerHTML(dateNow)
+    document.getElementsByClassName('.saveBtn').addEventListener('click', addUserInput());
+});
 //define the items to be retrieved as the items saved from class of description
 let apptDetails = JSON.parse(localStorage.getItem("description"))
 
 //runs the makeTimeSlot function over each business hour so that the time can be determined whether it is in the past present or future
-for (let i = 9; i <= 17; i++) {
-    $currentTime.append(makeTimeSlot(i));
+for (let i = 0; i <= 8; i++) {
+    makeTimeSlot(i);
 }
 
 //function for defining  past present and future
 function makeTimeSlot(hour) {
+    //make variable to govern background color
     var background;
     if (currentTime === hour){
         background = "present";
@@ -33,39 +79,36 @@ function makeTimeSlot(hour) {
     }
 }
 
-//define makeTimeSlot function
-function makeTimeSlot(hour){
+$("btn1").one("click", saveApt("#text1", "description"))
 
+//function to save appointment information to local storage
+function saveApt(event){
+    event.preventDefault();
+    addUserInput();
 }
-//make variable to govern background color
 
+//function to save user input to local storage when saveBtn is clicked
+function addUserInput(event){
+    event.preventDefault();
+    localStorage.setItem(inpKey1 && inpValue1, JSON.stringify);
+    return(localStorage);
+   }
 
-//Set key/value pairs with class rel 9am
-// var inpKey1 = document.getElementsById("#9");
-// var inpValue1 = document.getElementById("#text9");
-// document.getElementById("#btn1").addEventListener("click", addUserInput1(){
-//     console.log(addUserInput1);
-// });
+// Set key/value pairs with class rel 9am
+var inpKey1 = document.getElementsById("#9");
+var inpValue1 = document.getElementById("#text9");
+document.getElementById("#btn1").addEventListener("click", addUserInput1())
+    
+console.log("");
 
 // console.log(addUserInput1());
 //need for loop to collect all key and value pairs
 
-//Make sure everything loads in the page before save button is pressed
-document.addEventListener('load', ()=>{
-    document.getElementsById("#currentDay").display.innerHTML(dateNow)
-    document.getElementsByClassName('.saveBtn').addEventListener('click', addUserInput());
-});
 
-//function to save user input to local storage when saveBtn is clicked
-function addUserInput1(event){
- event.preventDefault();
- localStorage.setItem(inpKey1 && inpValue1, JSON.stringify);
- return(localStorage);
-}
         
 // function displayDateandTime() {
 //     if currentTime.value = time.value
         
 // // }
 console.log(currentTime);
-console.log(localStorage);
+// console.log(localStorage);
